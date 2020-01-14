3f7b3e553a05a3e7845442338c29469c707cc3d0
Lewis1304 <lewis.wright1304@gmail.com>
2020-01-14 14:54:15 +0000

    added text file for results

--- Qiskit_code/Modified_Gearbox_QC.py
+++ Qiskit_code/Modified_Gearbox_QC.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from qiskit.quantum_info.operators import Operator
 from qiskit.tools.monitor import job_monitor
+import csv
 
 def Initial_circuit(theta):
     # Build the initial-sub-circuit
@@ -52,11 +53,12 @@ def Subsequent_circuit(theta):
         sub_circ.cry(np.pi/2,sub_q[1],sub_q[2]) #reset gate
     return sub_circ.to_instruction()
 
-def experiment_QC(theta,N,name):
+def experiment_QC(theta,N,name,measure_type):
 
     register_qubits = QuantumRegister(N)
     state_qubit = QuantumRegister(1)
-    circ = QuantumCircuit(register_qubits,state_qubit)
+    c = ClassicalRegister(1)
+    circ = QuantumCircuit(register_qubits,state_qubit,c)
 
     RUS_initial = Initial_circuit(theta)
     RUS = Subsequent_circuit(theta)
@@ -65,40 +67,127 @@ def experiment_QC(theta,N,name):
 
     for i in range(1,N):
         circ.append(RUS, [register_qubits[i-1],register_qubits[i],state_qubit])
-    circ.measure_all()
 
-    #Simulate Circuit
-    IBMQ.load_account()
-    provider = IBMQ.get_provider('ibm-q')
-    qcomp = provider.get_backend(name)
+    if measure_type == 'x':
+        circ.h(state_qubit)
+    elif measure_type == 'y':
+        circ.sdg(state_qubit)
+        circ.h(state_qubit)
 
-    """
-    simulator = Aer.get_backend('qasm_simulator')
-    job = execute(circ, backend = simulator,shots = 8192)
-    result = job.result()
-    success_states = ['0'+'0'*(N-i) + '1'*i for i in range(N)] + ['1'+'0'*(N-i) + '1'*i for i in range(N)]
-    print(np.sum([result.get_counts(circ)[i] for i in success_states])/8192)
-    print(result.get_counts(circ))
-    """
+    circ.measure(state_qubit,c)
+    #circ.measure_all() #don't forget to input ClassicalRegister to change
 
-    #stats about the circuit implemented on the actual Quantum Computer- Different to theoretical circuit!:
-    t_circ = transpile(circ, backend=qcomp)
-    print('Circuit Depth: ',t_circ.depth())
-    print('Circuit Width: ',t_circ.width())
-    print('Total Number of Operations: ',t_circ.size())
-    print('Amount of paralell subcircuits: ',t_circ.num_tensor_factors())
+    if name == 'qasm_simulator':
+        simulator = Aer.get_backend(name)
+        job = execute(circ, backend = simulator,shots = 8192)
+        result = job.result()
+        result_dictionary = result.get_counts(circ)
 
-    job = execute(circ, backend = qcomp,shots = 8192)
-    print(job_monitor(job))
-    result = job.result()
-    result_dictionary = result.get_counts(circ)
+        probs = {}
+        for output in ['0','1']:
+            if output in result_dictionary:
+                probs[output] = result_dictionary[output]/8192
+            else:
+                probs[output] = 0
+        return probs['0'] -  probs['1']
 
-    success_states = ['0'+'0'*(N-i) + '1'*i for i in range(N)] + ['1'+'0'*(N-i) + '1'*i for i in range(N)]
-    Success_fidelity = np.sum([result_dictionary[i] for i in success_states])/8192
+    if name == 'qasm_simulator_fid':
+        simulator = Aer.get_backend('qasm_simulator')
+        job = execute(circ, backend = simulator,shots = 8192)
+        result = job.result()
+        result_dictionary = result.get_counts(circ)
 
-    return Success_fidelity
+        success_states = ['0'+'0'*(N-i) + '1'*i for i in range(N)] + ['1'+'0'*(N-i) + '1'*i for i in range(N)]
+        Success_fidelity = np.sum([result_dictionary[i] for i in success_states])/8192
+        return Success_fidelity
+
+    else:
+        #Simulate Circuit
+        IBMQ.load_account()
+        provider = IBMQ.get_provider('ibm-q')
+        qcomp = provider.get_backend(name)
+
+        #stats about the circuit implemented on the actual Quantum Computer- Different to theoretical circuit!:
+        #t_circ = transpile(circ, backend=qcomp)
+        #print('Circuit Depth: ',t_circ.depth())
+        #print('Circuit Width: ',t_circ.width())
+        #print('Total Number of Operations: ',t_circ.size())
+        #print('Amount of paralell subcircuits: ',t_circ.num_tensor_factors())
+
+        job = execute(circ, backend = qcomp,shots = 8192)
+        print(job_monitor(job))
+        result = job.result()
+        result_dictionary = result.get_counts(circ)
+
+        probs = {}
+        for output in ['0','1']:
+            if output in result_dictionary:
+                probs[output] = result_dictionary[output]/8192
+            else:
+                probs[output] = 0
+        return probs['0'] -  probs['1']
+        """
+        success_states = ['0'+'0'*(N-i) + '1'*i for i in range(N)] + ['1'+'0'*(N-i) + '1'*i for i in range(N)]
+        Success_fidelity = np.sum([result_dictionary[i] for i in success_states])/8192
+        return Success_fidelity
+        """
 
 
+acquiring_simulation_data = False
+if acquiring_simulation_data == True:
+    x = np.linspace(0,np.pi/2,25)
+    with open('data2.csv', 'w') as csvfile:
+
+        writer = csv.writer(csvfile, delimiter="\t")
+        writer.writerow(['Theta', 'Rounds', '<X>', '<Y>', '<Z>'])
+
+        for step,theta in enumerate(x):
+            for n_rounds in range(1,9):
+                    N = n_rounds
+                    X_exp = experiment_QC(theta,N,'qasm_simulator','x')
+                    Y_exp = experiment_QC(theta,N,'qasm_simulator','y')
+                    Z_exp = experiment_QC(theta,N,'qasm_simulator','z')
+                    writer.writerow([theta,n_rounds,X_exp,Y_exp,Z_exp])
+            print('Step: {} / 25'.format(step+1))
+
+
+
+acquiring_physical_data = False
+if acquiring_physical_data == True:
+    x = np.linspace(0,np.pi/2,5)
+    with open('data_physical.csv', 'w') as csvfile:
+
+        writer = csv.writer(csvfile, delimiter="\t")
+        writer.writerow(['Theta', 'Rounds', '<X>', '<Y>', '<Z>'])
+
+        for step,theta in enumerate(x):
+            for n_rounds in range(1,4):
+                    N = n_rounds
+                    X_exp = experiment_QC(theta,N,'ibmq_ourense','x')
+                    Y_exp = experiment_QC(theta,N,'ibmq_ourense','y')
+                    Z_exp = experiment_QC(theta,N,'ibmq_ourense','z')
+                    writer.writerow([theta,n_rounds,X_exp,Y_exp,Z_exp])
+            print('Step: {} / 5'.format(step+1))
+
+
+
+acquiring_physical_data_fid = False
+if acquiring_physical_data_fid == True:
+    x = np.linspace(0,np.pi/2,5)
+    with open('data_physical_fid_2.csv', 'w') as csvfile:
+
+        writer = csv.writer(csvfile, delimiter="\t")
+        writer.writerow(['Theta', 'Rounds', 'Fidelity'])
+
+        for step,theta in enumerate(x):
+            for n_rounds in range(1,4):
+                    N = n_rounds
+                    fidelity = experiment_QC(theta,N,'ibmq_ourense','o')
+                    writer.writerow([theta,n_rounds,fidelity])
+            print('Step: {} / 5'.format(step+1))
+
+
+"""
 N = 2 #number of rounds
 names = ['ibmq_5_yorktown','ibmq_essex','ibmq_ourense','ibmq_vigo','ibmq_burlington']
 fidelities = []
@@ -110,3 +199,9 @@ for i in names:
 print(fidelities)
 #fidelities_results = [0.6309814453125, 0.5810546875, 0.7349853515625, 0.7135009765625, 0.601806640625]
 #should be 0.75
+
+for theta in np.linspace(0,np.pi/4,4):
+    for n_rounds in [2]:
+        N = n_rounds
+        print(experiment_QC(theta,N,'ibmq_ourense'))
+"""
