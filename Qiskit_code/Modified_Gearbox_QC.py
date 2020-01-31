b2689467afe30dea2c6541069a5554c9907efd65
Lewis Wright <lewis.wright@kcl.ac.uk>
2020-01-31 17:53:36 +0000

    Corrected

--- Qiskit_code/Modified_Gearbox_QC.py
+++ Qiskit_code/Modified_Gearbox_QC.py
@@ -5,23 +5,6 @@ from qiskit.quantum_info.operators import Operator
 from qiskit.tools.monitor import job_monitor
 import csv
 
-def Initial_circuit(theta):
-    # Build the initial-sub-circuit
-
-    #Controlled -iY Gate
-    U_small = np.kron(np.diag([0,1]),[[0,-1],[1,0]]) + np.diag([1,1,0,0])
-    cU = Operator(U_small)
-
-    initial_sub_q = QuantumRegister(2)
-    initial_sub_circ = QuantumCircuit(initial_sub_q, name='initial_sub_circ')
-
-    initial_sub_circ.ry(2 * theta,initial_sub_q[0])
-    initial_sub_circ.unitary(cU, [initial_sub_q[1],initial_sub_q[0]], label='cU')
-    initial_sub_circ.ry(-2 * theta,initial_sub_q[0])
-    initial_sub_circ.cry(np.pi / 2,initial_sub_q[0],initial_sub_q[1])
-
-    return initial_sub_circ.to_instruction()
-
 #Controlled-Controlled -iY Gate, V^2 = -iY
 def custom_gate():
 
@@ -42,85 +25,62 @@ def custom_gate():
 
     return circuit.to_instruction()
 
-def Subsequent_circuit(theta):
-    # Build subsequent sub-circuit
-    sub_q = QuantumRegister(3)
-    sub_circ = QuantumCircuit(sub_q, name='sub_circ')
-    sub_circ.ry(2 * theta,sub_q[1])
-    sub_circ.append(custom_gate(),[sub_q[0],sub_q[1],sub_q[2]])
-    sub_circ.ry(-2 * theta,sub_q[1])
-    if N > 2:
-        sub_circ.cry(np.pi/2,sub_q[1],sub_q[2]) #reset gate
-    return sub_circ.to_instruction()
-
 def experiment_QC(theta,N,name,measure_type):
 
     register_qubits = QuantumRegister(N)
     state_qubit = QuantumRegister(1)
-    circ = QuantumCircuit(register_qubits,state_qubit)
+    c = ClassicalRegister(1)
+    circ = QuantumCircuit(register_qubits,state_qubit,c)
 
-    RUS_initial = Initial_circuit(theta)
-    RUS = Subsequent_circuit(theta)
 
-    circ.append(RUS_initial, [register_qubits[0],state_qubit])
 
+    #c-iY gate
+    U_small = np.kron(np.diag([0,1]),[[0,-1],[1,0]]) + np.diag([1,1,0,0])
+    cU = Operator(U_small)
+
+
+    #Initial Round
+    circ.ry(2 * theta,register_qubits[0])
+    circ.unitary(cU, [state_qubit,register_qubits[0]], label='cU') #target then control
+    circ.ry(-2 * theta,register_qubits[0])
+    circ.cry(np.pi/2,register_qubits[0],*state_qubit) #reset gate
+
+    #Subsequent Rounds
     for i in range(1,N):
-        circ.append(RUS, [register_qubits[i-1],register_qubits[i],state_qubit])
+        circ.ry(2 * theta,register_qubits[i])
+        circ.append(custom_gate(),[register_qubits[i-1],register_qubits[i],state_qubit]) #control then target
+        circ.ry(-2 * theta,register_qubits[i])
+        circ.cry(np.pi/2,register_qubits[i],*state_qubit) #reset gate
 
+
+    #Pauli Measurements
+    if measure_type == 'x':
+        circ.h(state_qubit)
+    elif measure_type == 'y':
+        circ.sdg(state_qubit)
+        circ.h(state_qubit)
+    elif measure_type == 'z':
+        circ.iden(state_qubit)
+    circ.measure(state_qubit,c)
 
     if name == 'qasm_simulator':
-
-        c = ClassicalRegister(1)
-        meas = QuantumCircuit(state_qubit,c)
-
-        if measure_type == 'x':
-            meas.h(state_qubit)
-        elif measure_type == 'y':
-            meas.sdg(state_qubit)
-            meas.h(state_qubit)
-
-        meas.measure(state_qubit,c)
-        circ += meas
-
+        shots = 50000
         simulator = Aer.get_backend(name)
-        job = execute(circ, backend = simulator,shots = 8192)
+        job = execute(circ, backend = simulator,shots = shots)
         result = job.result()
         result_dictionary = result.get_counts(circ)
 
         probs = {}
         for output in ['0','1']:
             if output in result_dictionary:
-                probs[output] = result_dictionary[output]/8192
+                probs[output] = result_dictionary[output]
             else:
                 probs[output] = 0
-        return probs['0'] -  probs['1']
 
-    if name == 'qasm_simulator_fid':
-        circ.measure_all()
-        simulator = Aer.get_backend('qasm_simulator')
-        job = execute(circ, backend = simulator,shots = 8192)
-        result = job.result()
-        result_dictionary = result.get_counts(circ)
-
-        success_states = ['0'+'0'*(N-i) + '1'*i for i in range(N)] + ['1'+'0'*(N-i) + '1'*i for i in range(N)]
-        Success_fidelity = np.sum([result_dictionary[i] for i in success_states])/8192
-        return Success_fidelity
+        return (probs['0'] -  probs['1']) / shots
 
     else:
-
-        c = ClassicalRegister(1)
-        meas = QuantumCircuit(state_qubit,c)
-
-        if measure_type == 'x':
-            meas.h(state_qubit)
-        elif measure_type == 'y':
-            meas.sdg(state_qubit)
-            meas.h(state_qubit)
-
-        meas.measure(state_qubit,c)
-        circ += meas
-
-        #Simulate Circuit
+        shots = 8192
         IBMQ.load_account()
         provider = IBMQ.get_provider('ibm-q')
         qcomp = provider.get_backend(name)
@@ -132,7 +92,7 @@ def experiment_QC(theta,N,name,measure_type):
         #print('Total Number of Operations: ',t_circ.size())
         #print('Amount of paralell subcircuits: ',t_circ.num_tensor_factors())
 
-        job = execute(circ, backend = qcomp,shots = 8192)
+        job = execute(circ, backend = qcomp,shots = shots)
         print(job_monitor(job))
         result = job.result()
         result_dictionary = result.get_counts(circ)
@@ -140,16 +100,19 @@ def experiment_QC(theta,N,name,measure_type):
         probs = {}
         for output in ['0','1']:
             if output in result_dictionary:
-                probs[output] = result_dictionary[output]/8192
+                probs[output] = result_dictionary[output]
             else:
                 probs[output] = 0
-        return probs['0'] -  probs['1']
+        return (probs['0'] -  probs['1']) /shots
 
+
+###############################################################################
+#Gathering Data
 
 acquiring_simulation_data = False
 if acquiring_simulation_data == True:
     x = np.linspace(0,np.pi/2,25)
-    with open('data2.csv', 'w') as csvfile:
+    with open('data_sim.csv', 'w') as csvfile:
 
         writer = csv.writer(csvfile, delimiter="\t")
         writer.writerow(['Theta', 'Rounds', '<X>', '<Y>', '<Z>'])
@@ -168,53 +131,16 @@ if acquiring_simulation_data == True:
 acquiring_physical_data = False
 if acquiring_physical_data == True:
     x = np.linspace(0,np.pi/2,5)
-    with open('data_physical.csv', 'w') as csvfile:
+    with open('data_physical4.csv', 'w') as csvfile:
 
         writer = csv.writer(csvfile, delimiter="\t")
         writer.writerow(['Theta', 'Rounds', '<X>', '<Y>', '<Z>'])
 
         for step,theta in enumerate(x):
-            for n_rounds in range(1,4):
+            for n_rounds in range(1,5):
                     N = n_rounds
                     X_exp = experiment_QC(theta,N,'ibmq_ourense','x')
                     Y_exp = experiment_QC(theta,N,'ibmq_ourense','y')
                     Z_exp = experiment_QC(theta,N,'ibmq_ourense','z')
                     writer.writerow([theta,n_rounds,X_exp,Y_exp,Z_exp])
             print('Step: {} / 5'.format(step+1))
-
-
-
-acquiring_physical_data_fid = False
-if acquiring_physical_data_fid == True:
-    x = np.linspace(0,np.pi/2,5)
-    with open('data_physical_fid.csv', 'w') as csvfile:
-
-        writer = csv.writer(csvfile, delimiter="\t")
-        writer.writerow(['Theta', 'Rounds', 'Fidelity'])
-
-        for step,theta in enumerate(x):
-            for n_rounds in range(1,4):
-                    N = n_rounds
-                    fidelity = experiment_QC(theta,N,'ibmq_ourense','o')
-                    writer.writerow([theta,n_rounds,fidelity])
-            print('Step: {} / 5'.format(step+1))
-
-
-"""
-N = 2 #number of rounds
-names = ['ibmq_5_yorktown','ibmq_essex','ibmq_ourense','ibmq_vigo','ibmq_burlington']
-fidelities = []
-for i in names:
-    round = experiment_QC(np.pi/4,N,i)
-    fidelities.append(round)
-    print(i)
-    print(round)
-print(fidelities)
-#fidelities_results = [0.6309814453125, 0.5810546875, 0.7349853515625, 0.7135009765625, 0.601806640625]
-#should be 0.75
-
-for theta in np.linspace(0,np.pi/4,4):
-    for n_rounds in [2]:
-        N = n_rounds
-        print(experiment_QC(theta,N,'ibmq_ourense'))
-"""
